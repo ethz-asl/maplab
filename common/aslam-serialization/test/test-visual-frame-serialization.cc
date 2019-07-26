fdd0d20e573442055cfc04a9fde369ac1a8b2be9
Juichung Kuo <raymond30031@gmail.com>
2019-07-26 13:15:42 +0200

    Extends serialization and deserialiation for visual frame semantic measurement channels Adds unit test for non-empty visual frame

--- common/aslam-serialization/test/test-visual-frame-serialization.cc
+++ common/aslam-serialization/test/test-visual-frame-serialization.cc
@@ -12,7 +12,7 @@
 
 namespace aslam {
 
-TEST(VisualFrameSerialization, SerializeDeserializeFrame) {
+TEST(VisualFrameSerialization, SerializeDeserializeEmptyVisualFrame) {
   const Camera::ConstPtr camera =
       aslam::PinholeCamera::createTestCamera<RadTanDistortion>();
   constexpr int64_t kTimestampNs = 0;
@@ -31,6 +31,56 @@ TEST(VisualFrameSerialization, SerializeDeserializeFrame) {
   serialization::deserializeVisualFrame(
       frame_proto, camera, &frame_deserialized);
   EXPECT_EQ(*frame, *frame_deserialized);
+}
+
+TEST(VisualFrameSerialization, SerializeDeserializeVisualFrame) {
+  const Camera::ConstPtr camera =
+      PinholeCamera::createTestCamera<RadTanDistortion>();
+  constexpr int64_t kTimestampNs = 12345678;
+  VisualFrame::Ptr frame =
+      VisualFrame::createEmptyTestVisualFrame(camera, kTimestampNs);
+
+  // Set some random Data.
+  constexpr size_t kNumRandomValues = 10;
+  Eigen::Matrix2Xd keypoints = Eigen::Matrix2Xd::Random(2, kNumRandomValues);
+  frame->setKeypointMeasurements(keypoints);
+  Eigen::VectorXd uncertainties = Eigen::VectorXd::Random(kNumRandomValues);
+  frame->setKeypointMeasurementUncertainties(uncertainties);
+  Eigen::VectorXd scales = Eigen::VectorXd::Random(kNumRandomValues);
+  frame->setKeypointScales(scales);
+  aslam::VisualFrame::DescriptorsT descriptors =
+      aslam::VisualFrame::DescriptorsT::Random(384, kNumRandomValues);
+  frame->setDescriptors(descriptors);
+  Eigen::VectorXi track_ids = Eigen::VectorXi::Random(kNumRandomValues);
+  frame->setTrackIds(track_ids);
+
+  Eigen::Matrix4Xi boxes = Eigen::Matrix4Xi::Random(4, kNumRandomValues);
+  frame->setSemanticObjectMeasurements(boxes);
+  Eigen::VectorXd sem_uncertainties = Eigen::VectorXd::Random(kNumRandomValues);
+  frame->setSemanticObjectMeasurementUncertainties(sem_uncertainties);
+  aslam::VisualFrame::SemanticObjectDescriptorsT sem_descriptors =
+      aslam::VisualFrame::SemanticObjectDescriptorsT::Random(4096, kNumRandomValues);
+  frame->setSemanticObjectDescriptors(sem_descriptors);
+  Eigen::VectorXi sem_class_ids = Eigen::VectorXi::Random(kNumRandomValues);
+  frame->setSemanticObjectClassIds(sem_class_ids);
+  Eigen::VectorXi sem_track_ids = Eigen::VectorXi::Random(kNumRandomValues);
+  frame->setSemanticObjectTrackIds(sem_track_ids);
+
+
+  proto::VisualFrame frame_proto;
+  serialization::serializeVisualFrame(*frame, &frame_proto);
+
+  VisualFrame::Ptr frame_deserialized;
+  serialization::deserializeVisualFrame(frame_proto, &frame_deserialized);
+
+  EXPECT_TRUE(frame->compareWithoutCameraGeometry(*frame_deserialized))<< "compareWithoutCameraGeometry test fail";
+
+  frame_deserialized.reset();
+  serialization::deserializeVisualFrame(
+      frame_proto, camera, &frame_deserialized);
+  
+
+  EXPECT_EQ(*frame, *frame_deserialized)<<"equal frame fail";
 }
 
 TEST(VisualFrameSerialization, SerializeDeserializeNFrame) {
