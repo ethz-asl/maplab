83737ab7a33048e69cd644305cf61517c72c8033
Juichung Kuo <raymond30031@gmail.com>
2019-08-15 12:00:14 +0200

    Removes experimental appearace feature from loop closure constraints

--- map-structure/vi-map/include/vi-map/loop-constraint.h
+++ map-structure/vi-map/include/vi-map/loop-constraint.h
@@ -32,42 +32,30 @@ struct FrameKeyPointToStructureMatch {
 struct VertexKeyPointToStructureMatch {
   VertexKeyPointToStructureMatch()
       : keypoint_index_query(-1),
-        frame_index_query(-1),
-        appearance(vi_map::Landmark::kInvalidAppearance) {}
+        frame_index_query(-1) {}
   VertexKeyPointToStructureMatch(
       unsigned int keypoint_index_query_, unsigned int frame_index_query,
       const vi_map::LandmarkId& landmark_result_)
       : keypoint_index_query(keypoint_index_query_),
         frame_index_query(frame_index_query),
-        landmark_result(landmark_result_),
-        appearance(vi_map::Landmark::kInvalidAppearance) {}
+        landmark_result(landmark_result_) {}
   VertexKeyPointToStructureMatch(
       unsigned int keypoint_index_query_, unsigned int frame_index_query,
-      const vi_map::LandmarkId& landmark_result_, int appearance_)
-      : keypoint_index_query(keypoint_index_query_),
-        frame_index_query(frame_index_query),
-        landmark_result(landmark_result_),
-        appearance(appearance_) {}
-  VertexKeyPointToStructureMatch(
-      unsigned int keypoint_index_query_, unsigned int frame_index_query,
-      const vi_map::LandmarkId& landmark_result_, int appearance_,
+      const vi_map::LandmarkId& landmark_result_,
       const vi_map::VisualFrameIdentifier& frame_identifier_result_)
       : keypoint_index_query(keypoint_index_query_),
         frame_index_query(frame_index_query),
         landmark_result(landmark_result_),
-        appearance(appearance_),
         frame_identifier_result(frame_identifier_result_) {}
   unsigned int keypoint_index_query;
   unsigned int frame_index_query;
   vi_map::LandmarkId landmark_result;
-  int appearance;
   vi_map::VisualFrameIdentifier frame_identifier_result;
   bool operator==(const VertexKeyPointToStructureMatch& other) const {
     bool result = true;
     result &= keypoint_index_query == other.keypoint_index_query;
     result &= frame_index_query == other.frame_index_query;
     result &= landmark_result == other.landmark_result;
-    result &= appearance == other.appearance;
     result &= frame_identifier_result == other.frame_identifier_result;
     return result;
   }
