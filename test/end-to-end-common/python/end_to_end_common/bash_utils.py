6643911d860fbe9945edab02d090d1b950a86ed8
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2019-12-15 22:49:45 +0100

    Added optional parameter to run tests with stdin input from a file

--- test/end-to-end-common/python/end_to_end_common/bash_utils.py
+++ test/end-to-end-common/python/end_to_end_common/bash_utils.py
@@ -5,13 +5,13 @@ import shlex
 import subprocess
 
 
-def run(cmd, dry_run=False):
+def run(cmd, dry_run=False, stdin=None):
     if dry_run:
         print("Would normally run:", cmd)
         return
     print("Running:", cmd)
     args = shlex.split(cmd)
-    proc = subprocess.Popen(args)
+    proc = subprocess.Popen(args, stdin=stdin)
     exit_code = proc.wait()
     if exit_code != 0:
         raise Exception("Cmd '%s' returned nozero exit code : %d" %
