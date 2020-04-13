c262d7788860f2b358e3a713d3dd5bacbc4bbe2b
Marius Fehr <mfehr@users.noreply.github.com>
2020-04-13 21:39:34 +0200

    Update common/maplab-common/include/maplab-common/temporal-buffer-inl.h
    Co-Authored-By: Thomas Schneider <schneith@ethz.ch>

--- common/maplab-common/include/maplab-common/temporal-buffer-inl.h
+++ common/maplab-common/include/maplab-common/temporal-buffer-inl.h
@@ -437,7 +437,8 @@ size_t TemporalBuffer<ValueType, AllocatorType>::
 
   // Get first iterator that is greater than the timestamp, i.e. everything
   // between begin() and this iterator is what we want.
-  auto first_newer_timestamp_it = values_.upper_bound(timestamp_ns);
+  typename BufferType::iterator first_newer_timestamp_it =
+		  values_.upper_bound(timestamp_ns);
 
   // Copy values earlier or equal to the timestamp.
   std::transform(
