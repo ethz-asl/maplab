d9abea63ea9b5cdb656d9310127444bbb423c9c4
Florian Tschopp <ftschopp@ethz.ch>
2020-04-20 18:23:11 +0200

    add the logo

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # html_theme = "alabaster"
-# html_logo = "logo.png"
+html_logo = "logo.png"
 
 #source_parsers = {
 #    '.md': 'recommonmark.parser.CommonMarkParser',
