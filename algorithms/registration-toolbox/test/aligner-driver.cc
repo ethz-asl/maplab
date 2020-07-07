191b80b240b8a68c52206c5f5d3039e01586a6e8
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-07-07 19:02:15 +0200

    tiny change in the regbox

--- algorithms/registration-toolbox/test/aligner-driver.cc
+++ algorithms/registration-toolbox/test/aligner-driver.cc
@@ -11,6 +11,7 @@
 DEFINE_string(target_cloud, "", "Defines the path to the target cloud.");
 DEFINE_string(source_cloud, "", "Defines the path to the source cloud.");
 DEFINE_string(reg_cloud, "", "Defines the path to the registered cloud.");
+DEFINE_string(transform, "", "Defines the path to the transformation.");
 
 namespace regbox {
 
@@ -36,9 +37,22 @@ static void writePointCloud(
   LOG(INFO) << "Wrote registered cloud to " << reg_file;
 }
 
+static void writeTransformToFile(
+    const std::string& transform,
+    const aslam::Transformation& T_target_source) {
+  CHECK(!transform.empty());
+  std::ofstream out_file(transform);
+
+  static const Eigen::IOFormat CSVFormat(
+      Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+  // Writes [w x y z, x y z]
+  out_file << T_target_source.asVector().transpose().format(CSVFormat) << "\n";
+  out_file.close();
+}
+
 static void registerCloud(
     const std::string& target, const std::string& source,
-    const std::string& reg_cloud) {
+    const std::string& reg_cloud, const std::string& transform) {
   pcl::PointCloud<pcl::PointXYZI>::Ptr target_cloud = readPointCloud(target);
   pcl::PointCloud<pcl::PointXYZI>::Ptr source_cloud = readPointCloud(source);
   CHECK_NOTNULL(target_cloud);
@@ -54,7 +68,12 @@ static void registerCloud(
       aligner->align(target_cloud, source_cloud, prior);
 
   LOG(INFO) << "Registration result: \n" << result.get_T_target_source();
-  writePointCloud(reg_cloud, result.getRegisteredCloud());
+  if (!reg_cloud.empty()) {
+    writePointCloud(reg_cloud, result.getRegisteredCloud());
+  }
+  if (!transform.empty()) {
+    writeTransformToFile(transform, result.get_T_target_source());
+  }
 }
 
 }  // namespace regbox
@@ -66,7 +85,7 @@ int main(int argc, char** argv) {
   google::InstallFailureSignalHandler();
 
   regbox::registerCloud(
-      FLAGS_target_cloud, FLAGS_source_cloud, FLAGS_reg_cloud);
+      FLAGS_target_cloud, FLAGS_source_cloud, FLAGS_reg_cloud, FLAGS_transform);
 
   return 0;
 }
