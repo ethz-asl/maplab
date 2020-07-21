a688c0e58a2ebf82f3a71f817d4a2722aec0465d
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-07-21 11:04:17 +0200

    added gicp run script for regbox

---
+++ algorithms/registration-toolbox/share/run_gicp.sh
@@ -0,0 +1,46 @@
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud001.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud002.ply "" "result001.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud003.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud004.ply "" "result002.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud005.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud006.ply "" "result003.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud007.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud008.ply "" "result004.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud009.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud010.ply "" "result005.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud011.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud012.ply "" "result006.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud013.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud014.ply "" "result007.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud015.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud016.ply "" "result008.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud017.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud018.ply "" "result009.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud019.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud020.ply "" "result010.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud021.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud022.ply "" "result011.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud023.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud024.ply "" "result012.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud025.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud026.ply "" "result013.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud027.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud028.ply "" "result014.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud029.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud030.ply "" "result015.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud031.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud032.ply "" "result016.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud033.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud034.ply "" "result017.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud035.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud036.ply "" "result018.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud037.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud038.ply "" "result019.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud039.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud040.ply "" "result020.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud041.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud042.ply "" "result021.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud043.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud044.ply "" "result022.txt" 
+
+./run_driver /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud045.ply /home/berlukas/Documents/ply_datasets/phaser_v2/experiments/exp-50db/2/ds_full/cloud046.ply "" "result023.txt" 
+
