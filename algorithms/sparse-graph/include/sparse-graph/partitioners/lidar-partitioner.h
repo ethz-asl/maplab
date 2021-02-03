732c3b353841e9c2742e538cac35c984d759729d
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-02-03 08:03:48 +0100

    init commit for the lidar partitioner

---
+++ algorithms/sparse-graph/include/sparse-graph/partitioners/lidar-partitioner.h
@@ -0,0 +1,27 @@
+#ifndef SPARSE_GRAPH_PARTITIONERS_LIDAR_PARTITIONER_H_
+#define SPARSE_GRAPH_PARTITIONERS_LIDAR_PARTITIONER_H_
+
+#include "sparse-graph/partitioners/base-partitioner.h"
+
+#include <unordered_map>
+
+namespace spg {
+
+class LidarPartitioner : public BasePartitioner {
+ public:
+  explicit LidarPartitioner(const vi_map::VIMap& map);
+  virtual ~LidarPartitioner() = default;
+  RepresentativeNode getRepresentativesForSubmap(
+      const pose_graph::VertexIdList& vertices) override;
+
+ private:
+  void initializeSensorMapping();
+
+  const backend::ResourceType point_cloud_resource_type_;
+  std::unordered_map<vi_map::MissionId, aslam::SensorId>
+      mission_to_lidar_sensor_map_;
+};
+
+}  // namespace spg
+
+#endif  // SPARSE_GRAPH_PARTITIONERS_LIDAR_PARTITIONER_H_
---
+++ algorithms/sparse-graph/src/partitioners/lidar-partitioner.cc
@@ -0,0 +1,78 @@
+#include "sparse-graph/partitioners/lidar-partitioner.h"
+
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+#include <Eigen/Geometry>
+
+#include <glog/logging.h>
+
+namespace spg {
+
+LidarPartitioner::LidarPartitioner(const vi_map::VIMap& map)
+    : BasePartitioner(map),
+      point_cloud_resource_type_(backend::ResourceType::kPointCloudXYZI) {
+  initializeSensorMapping()
+}
+
+void LidarPartitioner::initializeSensorMapping() {
+  vi_map::MissionIdList mission_ids;
+  map_.getAllMissionIds(&mission_ids);
+
+  if (mission_ids.empty()) {
+    LOG(ERROR) << "There are no missions in the loaded map. Aborting.";
+    return;
+  }
+
+  // Find the mapping between mission IDs and sensor IDs.
+  for (const vi_map::MissionId& mission_id : mission_ids) {
+    CHECK(mission_id.isValid());
+    const vi_map::VIMission& mission = map_->getMission(mission_id);
+    const SensorsToResourceMap* lidar_sensor_id_to_res_id_map =
+        mission.getAllSensorResourceIdsOfType(point_cloud_resource_type_);
+    if (lidar_sensor_id_to_res_id_map == nullptr) {
+      LOG(ERROR) << "Map has no appropriate lidar sensor associated with.";
+      return common::kUnknownError;
+    }
+    // We assume the first point cloud sensor is the correct one.
+    mission_to_lidar_sensor_map_.emplace(
+        mission_id, lidar_sensor_id_to_res_id_map->begin()->first);
+  }
+}
+
+RepresentativeNode LidarPartitioner::getRepresentativesForSubmap(
+    const pose_graph::VertexIdList& vertices) {
+  if (mission_to_lidar_sensor_map_.empty()) {
+    LOG(ERROR)
+        << "Initialization of the mission id to sensor map failed. Aborting.";
+    return RepresentativeNode();
+  }
+  const std::size_t n_vertices = vertices.size();
+  if (n_vertices == 0) {
+    LOG(ERROR) << "Received an empty vertex list. Aborting.";
+    return RepresentativeNode();
+  }
+
+  constexpr int64_t tolerance_ns = 1e8;  // 100ms
+  for (std::size_t i = 0u; i < n_vertices; ++i) {
+    const vi_map::Vertex& vertex = map_.getVertex(vertices[i]);
+    const vi_map::MissionId& mission_id = vertex.getMissionId();
+    const vi_map::VIMission& mission = map_.getMission(mission_id);
+    const int64_t ts_vertex_ns =
+        vertex.getVisualNFrame().getMinTimestampNanoseconds();
+
+    // Retrieve the closest LiDAR scan.
+    resources::PointCloud& pc;
+    int64_t ts_pc_ns;
+    map_->getClosestSensorResource(
+        mission_from, point_cloud_resource_type_,
+        mission_to_lidar_sensor_map_[mission_id], ts_vertex_ns, tolerance_ns,
+        &pc, &ts_pc_ns);
+  }
+
+  // Return averaged transformation with the used vertices.
+  // aslam::Transformation averaged_T(average_quaternion, average_position);
+  // return RepresentativeNode(averaged_T, vertices);
+  return RepresentativeNode();
+}
+
+}  // namespace spg
