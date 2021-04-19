97d4cace25769563980b74a99df9fa2d6dd3a8b7
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2021-04-19 18:32:44 +0300

    Added sensor serialization

--- map-structure/sensors/src/external-features.cc
+++ map-structure/sensors/src/external-features.cc
@@ -5,6 +5,11 @@
 namespace vi_map {
 
 constexpr char kYamlFieldNameTargetSensor[] = "target_sensor";
+constexpr char kYamlFieldNameHasUncertainties[] = "has_uncertainties";
+constexpr char kYamlFieldNameHasOrientations[] = "has_orientations";
+constexpr char kYamlFieldNameHasScores[] = "has_scores";
+constexpr char kYamlFieldNameHasScales[] = "has_scales";
+constexpr char kYamlFieldNameHasTrackIds[] = "has_track_ids";
 
 ExternalFeatures::ExternalFeatures()
     : Sensor(), target_camera_index_set_(false) {}
@@ -20,12 +25,21 @@ bool ExternalFeatures::loadFromYamlNodeImpl(const YAML::Node& sensor_node) {
       &id_as_string));
   CHECK(target_sensor_id_.fromHexString(id_as_string));
 
-  // TODO(smauq): fetch these actually
-  has_uncertainties_ = true;
-  has_orientations_ = true;
-  has_scores_ = true;
-  has_scales_ = true;
-  has_track_ids_ = true;
+  CHECK(YAML::safeGet(
+      sensor_node, static_cast<std::string>(kYamlFieldNameHasUncertainties),
+      &has_uncertainties_));
+  CHECK(YAML::safeGet(
+      sensor_node, static_cast<std::string>(kYamlFieldNameHasOrientations),
+      &has_orientations_));
+  CHECK(YAML::safeGet(
+      sensor_node, static_cast<std::string>(kYamlFieldNameHasScores),
+      &has_scores_));
+  CHECK(YAML::safeGet(
+      sensor_node, static_cast<std::string>(kYamlFieldNameHasScales),
+      &has_scales_));
+  CHECK(YAML::safeGet(
+      sensor_node, static_cast<std::string>(kYamlFieldNameHasTrackIds),
+      &has_track_ids_));
 
   feature_type_ = ExternalFeatureType::kVisualBinaryFeatures;
 
@@ -33,7 +47,18 @@ bool ExternalFeatures::loadFromYamlNodeImpl(const YAML::Node& sensor_node) {
 }
 
 void ExternalFeatures::saveToYamlNodeImpl(YAML::Node* sensor_node) const {
-  // TODO(smauq): write serializer
+  CHECK_NOTNULL(sensor_node);
+  YAML::Node& node = *sensor_node;
+  node[static_cast<std::string>(kYamlFieldNameTargetSensor)] =
+      target_sensor_id_.hexString();
+
+  node[static_cast<std::string>(kYamlFieldNameHasUncertainties)] =
+      has_uncertainties_;
+  node[static_cast<std::string>(kYamlFieldNameHasOrientations)] =
+      has_orientations_;
+  node[static_cast<std::string>(kYamlFieldNameHasScores)] = has_scores_;
+  node[static_cast<std::string>(kYamlFieldNameHasScales)] = has_scales_;
+  node[static_cast<std::string>(kYamlFieldNameHasTrackIds)] = has_track_ids_;
 }
 
 }  // namespace vi_map
