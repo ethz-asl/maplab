0e72bf0a9c04bcdd8ba95fdf304b1e1af6ba9e1b
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-09-01 22:16:59 +0200

    some other wip

--- algorithms/registration-toolbox/include/registration-toolbox/alignment/pcl-alignment.h
+++ algorithms/registration-toolbox/include/registration-toolbox/alignment/pcl-alignment.h
@@ -90,19 +90,19 @@ RegistrationResult PclAlignment<T_alignment, T_point>::registerCloudImpl(
 
   CHECK_GT(FLAGS_regbox_pcl_fitness_max_considered_distance_m, 0);
   CHECK_GT(FLAGS_regbox_pcl_max_fitness_score_m, 0);
-  const double fitness_score = aligner_.getFitnessScore(
-      FLAGS_regbox_pcl_fitness_max_considered_distance_m);
-  is_converged &= fitness_score <= FLAGS_regbox_pcl_max_fitness_score_m;
   const Eigen::Matrix4f T_f = aligner_.getFinalTransformation();
   const Eigen::Matrix4d T = T_f.cast<double>();
   const Eigen::MatrixXd cov = Eigen::MatrixXd::Identity(6, 6) * 1e-2;
 
-  if (T.topLeftCorner<3, 3>().normalized().squaredNorm() <=
-      static_cast<double>(1) +
-          kindr::minimal::EPS<double>::normalization_value()) {
+  if (is_converged &&
+      T.topLeftCorner<3, 3>().normalized().squaredNorm() <=
+          1.0 + kindr::minimal::EPS<double>::normalization_value()) {
+    const double fitness_score = aligner_.getFitnessScore(
+        FLAGS_regbox_pcl_fitness_max_considered_distance_m);
+    is_converged &= fitness_score <= FLAGS_regbox_pcl_max_fitness_score_m;
+    is_converged &= aligner_.hasConverged();
     return RegistrationResult(
-        transformed, cov, this->convertEigenToKindr(T),
-        aligner_.hasConverged() & is_converged);
+        transformed, cov, this->convertEigenToKindr(T), is_converged);
   } else {
     LOG(ERROR)
         << "PCL registration gave invalid rotation matrix, returning prior.";
