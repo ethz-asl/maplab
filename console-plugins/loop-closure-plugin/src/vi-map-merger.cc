ba8bc8c49a75ed54fecd227ab6d886c9a051d4ef
Andrei Cramariuc <andrei.cramariuc@gmail.com>
2022-07-14 18:54:14 +0200

    Added option to merge all mission landmarks and loop close against this single global map

--- console-plugins/loop-closure-plugin/src/vi-map-merger.cc
+++ console-plugins/loop-closure-plugin/src/vi-map-merger.cc
@@ -10,6 +10,12 @@ DEFINE_bool(
     lc_only_against_other_missions, false,
     "If true, no inter-mission loop-closures are sought.");
 
+DEFINE_bool(
+    lc_against_cumulative_map, false,
+    "Cumulate all landmarks simultaneously into the database and query "
+    "against that. Only to be used as a refinement step at the end, because "
+    "otherwise any misalignments will introduce bad associations.");
+
 VIMapMerger::VIMapMerger(
     vi_map::VIMap* map, const visualization::ViwlsGraphRvizPlotter* plotter)
     : map_(map), plotter_(plotter) {
@@ -38,22 +44,45 @@ int VIMapMerger::findLoopClosuresBetweenMissions(
     return common::kUnknownError;
   }
 
-  // We want to match all missions to all including every mission to
-  // itself to find loop-closures within the mission.
-  for (vi_map::MissionIdList::const_iterator it = mission_ids.begin();
-       it != mission_ids.end(); ++it) {
-    CHECK(it->isValid());
+  if (FLAGS_lc_against_cumulative_map) {
+    CHECK(!FLAGS_lc_only_against_other_missions)
+        << "When using a cumulative map all the missions are included in the "
+           "query database and queried against. They can not be separated.";
+
+    // Create joint global map of landmarks for queries
     loop_detector_node::LoopDetectorNode loop_detector;
     if (plotter_ != nullptr) {
       loop_detector.instantiateVisualizer();
     }
-    loop_detector.addMissionToDatabase(*it, *map_);
-    for (vi_map::MissionIdList::const_iterator jt = mission_ids.begin();
-         jt != mission_ids.end(); ++jt) {
-      if (FLAGS_lc_only_against_other_missions && *jt == *it) {
-        continue;
+
+    for (vi_map::MissionIdList::const_iterator it = mission_ids.begin();
+         it != mission_ids.end(); ++it) {
+      CHECK(it->isValid());
+      loop_detector.addMissionToDatabase(*it, *map_);
+    }
+
+    // Attempt to merge landmarks from each mission against the entire database
+    for (vi_map::MissionIdList::const_iterator it = mission_ids.begin();
+         it != mission_ids.end(); ++it) {
+      loop_detector.detectLoopClosuresAndMergeLandmarks(*it, map_);
+    }
+  } else {
+    // We want to try to loop close every mission pair
+    for (vi_map::MissionIdList::const_iterator it = mission_ids.begin();
+         it != mission_ids.end(); ++it) {
+      CHECK(it->isValid());
+      loop_detector_node::LoopDetectorNode loop_detector;
+      if (plotter_ != nullptr) {
+        loop_detector.instantiateVisualizer();
       }
-      loop_detector.detectLoopClosuresAndMergeLandmarks(*jt, map_);
+      loop_detector.addMissionToDatabase(*it, *map_);
+      for (vi_map::MissionIdList::const_iterator jt = mission_ids.begin();
+           jt != mission_ids.end(); ++jt) {
+        if (FLAGS_lc_only_against_other_missions && *jt == *it) {
+          continue;
+        }
+        loop_detector.detectLoopClosuresAndMergeLandmarks(*jt, map_);
+      }
     }
   }
 
