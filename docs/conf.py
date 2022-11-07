67d8f2e8588870087cee2034c291137eda7eefb2
Andrei Cramariuc <andrei.cramariuc@gmail.com>
2022-11-07 19:03:08 +0100

    Included devel branch in wiki versioning

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ templates_path = ["_templates"]
 # Check https://holzhaus.github.io/sphinx-multiversion/master/
 #               configuration.html#tag-branch-remote-whitelists
 smv_tag_whitelist = None  # Include no tags
-smv_branch_whitelist = r'^(master)$'  # Everything is on master now
+smv_branch_whitelist = r'^(master|wiki_update)$'  # Everything is on master now
 smv_remote_whitelist = r'^(origin|upstream)$'  # Use origin and upstream
 
 extensions = [
