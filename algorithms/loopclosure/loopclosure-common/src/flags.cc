e2fb04e684677a6dc8b159f3430848a7be9258b6
Andrei Cramariuc <andrei.cramariuc@gmail.com>
2023-04-19 14:20:09 +0200

    Tightened parameter a bit to improve BRISK/FREAK descriptor search

--- algorithms/loopclosure/loopclosure-common/src/flags.cc
+++ algorithms/loopclosure/loopclosure-common/src/flags.cc
@@ -7,7 +7,7 @@ DEFINE_string(
     feature_descriptor_type, loop_closure::kFeatureDescriptorBRISK,
     "The feature descriptors to use. [brisk, freak]");
 DEFINE_double(
-    lc_knn_epsilon, 3.0,
+    lc_knn_epsilon, 2.0,
     "Epsilon approximation value for the nearest neighbor search.");
 DEFINE_double(
     lc_knn_max_radius, 20.0, "Max radius for the nearest neighbor search.");
