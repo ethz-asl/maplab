da24f308451a7665d1f4392c9efc536d202c969e
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2023-06-11 17:38:39 +0200

    Moved the ICP based registration toolbox to maplab_extras

--- algorithms/registration-toolbox/share/build_and_test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-catkin build registration_toolbox --no-deps && catkin run_tests registration_toolbox --no-deps && catkin_test_results ../../../../../build/registration_toolbox
