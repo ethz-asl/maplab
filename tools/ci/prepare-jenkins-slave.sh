fa11ad4f5cef48e20bef710d3c6129efb7210548
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2023-06-12 02:03:37 +0200

    Fixed apt command for jenkins

--- tools/ci/prepare-jenkins-slave.sh
+++ tools/ci/prepare-jenkins-slave.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 echo "Running the prepare script for maplab.";
 
-sudo apt install -y autotools-dev ccache doxygen dh-autoreconf git \
-                  liblapack-dev libblas-dev libgtest-dev libreadline-dev \
-                  libssh2-1-dev libatlas3-base libv4l-dev libjpeg-dev \
-                  python3 python3-catkin-tools python3-pip python-git-doc \
-                  python3-setuptools python3-termcolor python3-wstool
+sudo apt-get install -y autotools-dev ccache doxygen dh-autoreconf git \
+                        liblapack-dev libblas-dev libgtest-dev libreadline-dev \
+                        libssh2-1-dev libatlas3-base libv4l-dev libjpeg-dev \
+                        python3 python3-catkin-tools python3-pip python-git-doc \
+                        python3-setuptools python3-termcolor python3-wstool
 
 # Python package for end to end test
 pip3 install --user --upgrade pip
